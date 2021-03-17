014dc3d28663fe1d15ae24673e91984b03c6ee95
Hank <hank@jesda.com.tw>
2021-03-17 15:19:38 +0800

    feat: 調整登入頁面部分邏輯
    移除首頁跳頁功能
    調整 form 表單標籤
    添加 csrf 機制

--- resources/views/Admin/login.blade.php
+++ resources/views/Admin/login.blade.php
@@ -17,24 +17,25 @@
 <body class="hold-transition login-page">
 <div class="login-box">
     <div class="login-logo">
-        <a href={{asset('admin-layout/index2.html')}}><b>YoYoAdmin</b></a>
+        <a href='#'><b>YoYoAdmin</b></a>
     </div>
     <!-- /.login-logo -->
     <div class="card">
         <div class="card-body login-card-body">
             <p class="login-box-msg">Sign in to start your session</p>
 
-            <form action={{asset('admin-layout/index3.html" method="post')}}>
+            <form action={{asset('admin/login')}} method="post">
+                @csrf
                 <div class="input-group mb-3">
-                    <input type="email" class="form-control" placeholder="Email">
+                    <input type="account" name="account" class="form-control" placeholder="Account">
                     <div class="input-group-append">
                         <div class="input-group-text">
-                            <span class="fas fa-envelope"></span>
+                            <span class="fas fa-user"></span>
                         </div>
                     </div>
                 </div>
                 <div class="input-group mb-3">
-                    <input type="password" class="form-control" placeholder="Password">
+                    <input type="password" name="password" class="form-control" placeholder="Password">
                     <div class="input-group-append">
                         <div class="input-group-text">
                             <span class="fas fa-lock"></span>
